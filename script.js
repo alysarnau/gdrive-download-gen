c8986207f0a16b1191baf6df68e3d1a0b3334e6d
alysvolatile <alice.g.cooper@gmail.com>
2022-09-29 16:35:07 -0400

    Set up google download link logic

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// need to create logic for "generate direct download link" button - not a form
+// example link
+// https://drive.google.com/file/d/1trbB_p0-T5MM7KggduaBVpCxNS0a_n33/view?usp=sharing
+
+let googleLinkUrlInput = document.querySelector("#glink")
+let generateLinkButton = document.querySelector("#btn")
+let downloadLink = document.querySelector("#download-link")
+// store original gDrive link
+let googleDriveLink;
+// variables to populate 
+let audioEmbedLink;
+let videoEmbedLink;
+
+
+generateLinkButton.addEventListener("click", generateLink)
+
+function generateLink(e) {
+    e.preventDefault();
+    googleDriveLink = googleLinkUrlInput.value
+    const confirmLink = googleDriveLink.includes("https://drive.google.com/file/d/")
+    if (!confirmLink) {
+        alert('Please use a valid Google Drive link')
+        return;
+    } 
+    const getDownloadLink = googleDriveLink
+        .replace("https://drive.google.com/file/d/", "https://drive.google.com/uc?export=download&id=")
+        .replace("/view?usp=sharing", "")
+    console.log(getDownloadLink)
+    downloadLink.value = getDownloadLink
+}
